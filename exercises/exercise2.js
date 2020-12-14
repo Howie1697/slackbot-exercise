6a4fe046801dac768da08216bd9ce4c8e3498171
Howard Gilmore <Howardgg1997@gmail.com>
2020-12-13 22:26:10 -0800

    My functional bot

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -1,31 +1,53 @@
 // Set this assignment to true if you do want to use it.
-module.exports.ACTIVATE_BOT = false;
+module.exports.ACTIVATE_BOT = true;
 
 module.exports.botScripts = [
   // Write an interaction where one user says "I'm tired", the bot says "Wake up!"
   //
   // mychatbot> I'm tired
   // mychatbot responds "Wake up!"
-  
+  {
+    label: 'Lets try this',
+    prompt: "I'm tired",
+    handler: function () {
+      return 'Wake up!';
+    },
+    isReply: false,
+    isCaseSensitive: false,
+    isListening: false,
+  },
 
-
-
-
-  // Write an interaction where one user asks the bot 
+  // Write an interaction where one user asks the bot
   // "What's the area of a circle with radius X?" where X is any number
   // The chatbot will respond to the user with the answer.
-  // 
+  //
   // mychatbot> mychatbot What's the area of a circle with radius 5?
   // mychatbot responds to user "The area is 78.5398...."
-  
+  {
+    label: 'This script will give us a formula',
+    prompt: 'Whats the area of a circle with radius #{anyNumber}',
+    handler: function (params) {
+      const area = Math.PI * params.anyNumber ** 2;
+      return 'The area is ' + Math.ceil(area);
+    },
+  },
 
-
-
-
-  // Write an interaction where one user asks the bot 
+  // Write an interaction where one user asks the bot
   // "Tell me a story with NAME1 and NAME2" where NAME1 and NAME2 is any string
   // The chatbot will respond with the answer.
-  // 
+  //
   // mychatbot> mychatbot Tell me a story with Jack and Jill
   // mychatbot responds to user "Jack and Jill went up the hill to fetch a pail of water."
+  {
+    label: 'This will be a story about =3 and cody jhonson',
+    prompt: 'Tell me a story with #{anyName1} and #{anyName}',
+    handler: function (params) {
+      const storyTeller =
+        params.anyName1 +
+        ' and ' +
+        params.anyName +
+        ' needed some water but it was a desert and oasis are arent real.';
+      return storyTeller;
+    },
+  },
 ];
--- exercises/exercise3.js
+++ exercises/exercise3.js
@@ -1,5 +1,5 @@
 // Set this assignment to true if you do want to use it.
-module.exports.ACTIVATE_BOT = false;
+module.exports.ACTIVATE_BOT = true;
 
 module.exports.botScripts = [
   {
@@ -8,5 +8,145 @@ module.exports.botScripts = [
     handler: function () {
       return 'Good Evening to You!';
     },
+    //I want the bot to find the to be have a varity of moods when asked a question.
+
+    label: 'Whats your mood bot',
+    prompt: 'Next are you happy',
+    handler: function () {
+      return 'Yes i am';
+    }, //this will tell me what my name is
+    label: 'I want to make a discord bot',
+    prompt: 'What is my #{name}',
+    handler: function (params) {
+      const customer = params.name;
+      return 'Your name is ' + params.name;
+    },
+    isCaseSensitive: false,
+    isListening: true,
+  },
+  {
+    //This will return the users status if he is online or not
+    label: 'Whats my status',
+    prompt: 'Am offline or online?',
+    handler: function () {
+      const onlineStatus = 20;
+      const offlineStatus = 75;
+      const num = 50;
+      if (onlineStatus < 50) {
+        return 'You are online!';
+      } else if (offlineStatus > 50) {
+        return 'You are offline!';
+      } else {
+        return 'You need better internet.....';
+      }
+    },
+    isCaseSensitive: false,
+    isListening: true,
+  },
+  {
+    label: 'This is a magic eight ball buddy',
+    prompt: 'My name is #{userName}, whats my destiny',
+    handler: function (params) {
+      let userName = params.userName;
+      userName
+        ? console.log('Hello,' + userName + ' !')
+        : console.log('Hello!');
+      const randomNumber = Math.floor(Math.random() * 8);
+      let eightBall = '';
+      switch (randomNumber) {
+        case 0:
+          eightBall = 'Reconsider this question';
+          break;
+        case 1:
+          eightBall = 'Here we go agian...';
+          break;
+        case 2:
+          eightball = 'Really dude? why... like why...';
+          break;
+        case 3:
+          eightBall = 'I dont get paid enough for this';
+          break;
+        case 4:
+          eightBall = 'Its not what your doing right now bud.';
+          break;
+        case 5:
+          eightBall = 'Eh, maybe you have some purpose';
+          break;
+        case 6:
+          eightBall = 'You can be a sailor';
+          break;
+        case 7:
+          eightBall = 'Just be a programmer and live happy!';
+          break;
+      }
+      console.log(eightBall);
+    },
+    isCaseSensitive: false,
+    isListening: true,
+    isreply: true,
+  },
+  {
+    label: 'What is 2 + 2',
+    prompt: ' whats #{number} + #{number1}',
+    handler: function (params) {
+      if (params.number === '' && params.number1 === '') {
+        return '';
+      } else {
+        return null;
+      }
+    },
+    isCaseSensitive: false,
+    isListening: true,
+    isreply: true,
+  },
+  {
+    label: 'Hard day',
+    prompt: 'today was rough',
+    handler: function () {
+      return 'im sorry, would you like to talk about it?';
+    },
+
+    isCaseSensitive: false,
+    isListening: true,
+    isreply: true,
+  },
+  {
+    label:
+      'Its race day! If you registered at a certain time come grab your time and number!',
+    prompt: 'what time do i race and whats my number?',
+    handler: function () {
+      let raceNumber = Math.floor(Math.random() * 1000);
+      let registeredEarly = true;
+      let runnersAge = '18';
+      if ((runnersAge += '18' && registeredEarly)) {
+        console.log(
+          'Your number is' +
+            ' ' +
+            raceNumber +
+            '. ' +
+            'You may start your engines!!!!!'
+        );
+      } else if ((runnersAge += '18' && !registeredEarly)) {
+        console.log('We are sorry you will race at 11:00am');
+      } else if (!runnerAge < '18') {
+        console.log(raceNumber + ' ' + ' You will race at 12:30');
+      } else {
+        console.log('Please head to your registration table');
+      }
+    },
+    isCaseSensitive: false,
+    isListening: true,
+    isreply: true,
+  },
+  {
+    label: 'Im very tired and crammed with homework what do i do',
+    prompt: 'im very tired what do i do?',
+    handler: function () {
+      return 'You tried man, you passed your final pre-calc exam with an A and submitted a 10 page essay. goto bed.';
+    },
+
+    isCaseSensitive: false,
+    isListening: true,
+    isreply: true,
   },
 ];
